fb35beef31755e10a70699eb91bef88a743d5fec
Rostyslav Ignatenko <r.ignatenko@aimprosoft.com>
2017-02-22 17:35:35 +0200

    tests for order status are added

--- src/test/java/Geempower/DefinitionTestSuite.java
+++ src/test/java/Geempower/DefinitionTestSuite.java
@@ -6,11 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features={
-//        "src/test/resources/features/AccountManagement.feature",
+        "src/test/resources/features/AccountManagement.feature",
 //        "src/test/resources/features/Dashboard.feature",
-//        "src/test/resources/features/SavedLists.feature",
-//        "src/test/resources/features/PriceAndAvailability.feature",
-//        "src/test/resources/features/OrderEntry.feature",
+        "src/test/resources/features/SavedLists.feature",
+        "src/test/resources/features/PriceAndAvailability.feature",
+        "src/test/resources/features/OrderEntry.feature",
         "src/test/resources/features/OrderStatus.feature"
 })
 public class DefinitionTestSuite {}
